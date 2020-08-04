224c019a51ee0bb77963693a7aa60e01152c6307
Felipe <felipe.sth@gmail.com>
2020-08-04 09:49:45 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Squirrel;
+
+namespace MyApp
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static async Task Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            using (var mgr = new UpdateManager(@"C:\Users\Felipe-Sistema\Source\Repos\MyApp\Releases"))
+            {
+                await mgr.UpdateApp();
+            }
+            Application.Run(new Form1());
+        }
+    }
+}
